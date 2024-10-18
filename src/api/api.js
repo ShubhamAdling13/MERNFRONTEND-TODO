dfcaf0bf1f940d09497e466bf5cf1ac6044c90c5
Shubham Adling <shubhamadling13@gmail.com>
2024-10-18 12:13:21 +0530

    added edit note feature frontend api change

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
-export const apiurl = 'http://localhost:2000';
+// export const apiurl = 'http://localhost:2000';
 
-// export const apiurl = "https://mernbackend-todo.onrender.com";
+export const apiurl = "https://mernbackend-todo.onrender.com";
 // export const apiurl = process.env.REACT_APP_API_URL;
